9cddbc390321438b068f6e5ffb57ffaad2fd6565
Tim <72595331+Pixeltrace-GER@users.noreply.github.com>
2022-11-09 07:44:48 +0100

    Working on python (#1)
    * Create run.py
    * Create requirements.txt
    * Update .gitignore

---
+++ data/run.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import os, re
+import pysondb
+
+
+def find_files(root, ext):
+  for root, dirs, files in os.walk(root):
+    for f in files:
+      if f.lower().endswith(ext):
+        f = os.path.join(root, f)
+        yield f
+
+    for d in dirs:
+      d = os.path.join(root, d)
+      find_files(d, ext)
+      
+      
+def replace_regex(text):
+  print(re.sub("'(?:[^\\']|\\\\|\\')*'", "", text))
+      
+def find_regex(line):
+  return re.findall(r"'(?:[^\\']|\\\\|\\')*'", line)
+
+
+
+if __name__ == '__main__':
+  for f in find_files('/src', '.js'):
+    print(f)
+    
